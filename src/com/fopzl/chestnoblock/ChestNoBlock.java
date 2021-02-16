d6a3fc0213de0a6335bfff5f4d05789193f30d82
FoPzl <officialsuperalex11@gmail.com>
2021-02-16 06:17:44 -0500

    Implemented Basic Functionality

--- src/com/fopzl/chestnoblock/ChestNoBlock.java
+++ src/com/fopzl/chestnoblock/ChestNoBlock.java
@@ -5,6 +5,32 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.block.Block;
+import org.bukkit.Location;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.block.data.type.Chest;
+import org.bukkit.entity.Player;
+
 public class ChestNoBlock extends JavaPlugin implements Listener {
-    // TODO
+    public void onEnable(){
+        super.onEnable();
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+    
+    @EventHandler(ignoreCancelled = true)
+    public void onBlockPlace(BlockPlaceEvent event){
+        Block b = event.getBlockPlaced();
+        
+        if(event.getPlayer().isOp()){
+            return;
+        }
+        
+        if(!(b.getRelative(0, -1, 0).getBlockData() instanceof Chest)){
+            return;
+        }
+        
+        event.setBuild(true);
+        event.setCancelled(true);
+    }
 }
