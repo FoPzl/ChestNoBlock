5dfe63ae2e9fc6d2e3f8cbf07d90f0cd5a51f365
FoPzl <officialsuperalex11@gmail.com>
2021-02-16 05:43:50 -0500

    Initial Commit

---
+++ src/com/fopzl/chestnoblock/ChestNoBlock.java
@@ -0,0 +1,10 @@
+package com.fopzl.chestnoblock;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ChestNoBlock extends JavaPlugin implements Listener {
+    // TODO
+}
